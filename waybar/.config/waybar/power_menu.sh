62b6dfc56c2c9be277e572562984cae2003a0549
calendar <1131821081@qq.com>
2025-12-11 09:00:53 +0800

    init arch sway environment

---
+++ waybar/.config/waybar/power_menu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 定义选项 (图标 + 文字)
+# 注意：这里使用的是 Nerd Font 图标
+lock="  Lock"
+logout="  Logout"
+suspend="  Suspend"
+reboot="  Reboot"
+shutdown="  Shutdown"
+
+# 构建菜单内容
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+# 调用 Wofi 显示菜单
+# --dmenu: 只有文字列表模式
+# --cache-file /dev/null: 不记录历史，每次顺序固定
+selected=$(echo -e "$options" | wofi --dmenu --width 200 --height 200 --cache-file /dev/null --prompt "Power Menu")
+
+# 根据选择执行命令
+case $selected in
+    "$lock")
+        swaylock -f -i /home/calendar/Pictures/background/壁纸.jpg -s fill
+        ;;
+    "$suspend")
+        systemctl suspend
+        ;;
+    "$logout")
+        swaymsg exit
+        ;;
+    "$reboot")
+        systemctl reboot
+        ;;
+    "$shutdown")
+        systemctl poweroff
+        ;;
+esac
